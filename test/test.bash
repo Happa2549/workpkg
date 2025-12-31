2238ac1bf649e80dbb3ca048698d7351e293bb21
Happa2549 <s24c1122vn@s.chibakoudai.jp>
2025-12-31 11:29:44 +0900

    modification of test code

--- test/test.bash
+++ test/test.bash
@@ -3,16 +3,15 @@ set -e
 
 WORKSPACE=/github/workspace/ros2_ws
 
-cd $WORKSPACE
-mkdir -p src
-cd src
+mkdir -p $WORKSPACE/src
 cd $WORKSPACE
 
 source /opt/ros/humble/setup.bash
-colcon build
+
+colcon build --symlink-install
 source install/setup.bash
 
-bash -c "timeout 5 ros2 run workpkg task2_test > /tmp/task2.log 2>&1 || true" &
+timeout 5 ros2 run workpkg task2_test > /tmp/task2.log 2>&1 &
 
 echo "test task" | ros2 run workpkg task1_test
 
@@ -26,4 +25,5 @@ else
     cat /tmp/task2.log
     exit 1
 fi
+
 
