41fc15bf2b69f57b9979921092fb5287c0d27026
Happa2549 <s24c1122vn@s.chibakoudai.jp>
2025-12-31 20:09:12 +0900

    remake a test

--- test/test.bash
+++ test/test.bash
@@ -15,28 +15,17 @@ source /opt/ros/humble/setup.bash
 colcon build --symlink-install
 source install/setup.bash
 
-TMP_LOG=/tmp/task2.log
 
-timeout 5 ros2 run workpkg task2_test > $TMP_LOG 2>&1 &
+timeout 5 ros2 run workpkg task2_test > /tmp/task2.log 2>&1 &
 echo "test task" | ros2 run workpkg task1_test
 sleep 1
-if grep -q "test task" $TMP_LOG; then
+if grep -q "test task" /tmp/task2.log; then
 	echo "Test passed" 
 	exit 0 
 else echo "Test failed"
-	cat $TMP_LOG 
+	cat /tmp/task2.log
 	exit 1 
 fi
 
-echo "single message" | ros2 run workpkg task1_test
-if [ $? -eq 0 ]; then
-    echo "[task1_test single message] Passed"
-else
-    echo "[task1_test single message] Failed"
-    exit 1
-fi
-
-echo "All tests passed"
-exit 0
 
 
